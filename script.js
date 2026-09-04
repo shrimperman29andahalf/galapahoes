252868cbcc9a4fac4a40840b98a63df12e0fe55f
shrimperman29andahalf <streaks-71.bills@icloud.com>
2026-09-04 13:00:23 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function showDemo(name) {
-  const toast = document.getElementById("toast");
-  toast.textContent = `${name} will become functional in Version 2.`;
-  toast.classList.add("show");
-  clearTimeout(window.toastTimer);
-  window.toastTimer = setTimeout(() => toast.classList.remove("show"), 2800);
-}
